eef85dd23e47cf38f3e65f216873c879b526a30b
ellataira <ejtaira177@gmail.com>
2024-09-10 20:45:28 -0400

    let's try gh actions

---
+++ paddle/data.py
@@ -0,0 +1,22 @@
+from Reservation import Reservation
+
+class data:
+    # CODE = "35780"
+    # DAY = ["1", "26"]  # [month, converted_day]
+    # TIME = "13:30"
+    # COURT_INDEX = "2"
+    # NAME0 = "Taira, Kelly"
+    # NAME1 = "Krause, Ron"
+    # NAME2 = "Hoyt, Mark"
+
+    COURT1 = Reservation("35780", ["3", "29"], "16:30", "1", "Taira, Kelly", "Hoyt, Mark", "Iammartino, Mark")
+
+    # CODE = "11682"
+    # DAY = ["1", "26"]  # [month, converted_day]
+    # TIME = "13:30"
+    # COURT_INDEX = "2"  ## once a court is filled, it is no longer in the court list <td>s (it has become <th>),
+    # NAME0 = "Flodin, TJ"
+    # NAME1 = "Lundak, Dan"
+    # NAME2 = "Condon, Sean"
+
+    COURT2 = Reservation("11682", ["3", "29"], "16:30",  "1", "Krause, Ron", "Lundak, Dan", "Burns, Mike")
